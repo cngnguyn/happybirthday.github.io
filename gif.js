ca390d60e19c4353cc0be4e0f7317a1c1d92d1a0
Cuong Viet Nguyen <92213091+cngnguyn@users.noreply.github.com>
2022-11-18 17:56:40 +0700

    happybirthday
    HappyBirthdaytoHaChi

---
+++ gif.js
@@ -0,0 +1,44 @@
+var to = 'Hà Chiii :3';
+var gift_url = 'hearttohachi.html';
+var gift_image_url = 'ảnh cute.jpg';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
